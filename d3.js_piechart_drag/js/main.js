1bbaec4ea5e10afdc9685dc22f94dd15e62b20f5
jaswu51 <506346013@qq.com>
2022-12-29 01:41:25 +0100

    push

--- d3.js_piechart_drag/js/main.js
+++ d3.js_piechart_drag/js/main.js
@@ -1,35 +1,91 @@
-data= [
-  { x: 10, y: 10, r:10,color: "red" },
-  { x: 40, y: 40, r:10,color: "yellow" },
-  { x: 70, y: 70, r:10,color: "blue" }
+const data = [
+  { name: "外包", value: 3000 },
+  { name: "金融", value: 4754 },
+  { name: "制造", value: 1120 },
+  { name: "咨询", value: 4032 }
 ];
-svg=d3.select('#piechart').append("svg")
-.attr("width", document.body.clientWidth)
-.attr("height", document.body.clientWidth);
 
-svg.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .style("stroke", "gray")
-    .style("fill", "black")
-    .attr("r", d=>d.r)
-    .attr("cx",  d=>d.x)
-    .attr("cy",  d=>d.y)
-    .on("mouseover", function (d) {d3.select(this).style("cursor", "move");})
-    .on("mouseout", function (d) {})
-    .call(d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended));
-function dragstarted(event) {
-      d3.select(this).raise().classed("active", true);
-  }
+var width = 450,
+    height = 450,
+    outRadius=100,
+    outRadius1=60;
 
-  function dragged(event) {
-      d3.select(this).attr("cx", x = event.x).attr("cy", y = event.y);
-  }
+const svg=d3.select("#piechart")
+.append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
-  function dragended(event) {
-    d3.select(this).classed("active", false);
+const pieData = d3.pie()
+    .value((d) => d.value)
+    .sort((a, b) => a.value - b.value)(data);
+
+const arc = d3.arc().innerRadius(0).outerRadius(outRadius);
+const arc1 = d3.arc().innerRadius(0).outerRadius(outRadius1);
+const color = d3.scaleOrdinal(d3.schemeCategory10);
+const color1 = d3.scaleLinear().domain([1,10])
+.range(["white", "blue"]);
+
+
+
+const arcGroup = svg.append("g")
+    .attr("id", "g1")
+    .attr("transform", "translate(300, 300)")
+    .selectAll("path")
+    .data(pieData);
+
+
+arcGroup.join("path")
+    .attr("fill", (d, i) => color(i))
+    .attr("d", arc);
+
+const arcGroup1 = svg.append("g")
+    .attr("id", "g2")
+    .attr("transform", "translate(120, 100)")
+    .call(
+      d3.drag()
+    .on("start", dragstarted)
+    .on("drag", dragged)
+    .on("end", dragended)
+    )
+    .selectAll("path")
+    .data(pieData)
+    ;
+arcGroup1.join("path")
+    .attr("fill", (d, i) => color1(i))
+    .attr("d", arc1)
+    ;
+
+function update() {
+  voronoi = d3.Delaunay.from(g, d => d.x, d => d.y).voronoi([0, 0, width, height]);
+  circle.attr("transform", d => `translate(${d.x},${d.y})`);
+  cell.attr("d", (d, i) => voronoi.renderCell(i));
+}
+
+
+
+
+function dragstarted(_event) {
+    d3.select(this).raise().classed("active", true);
+}
+
+function dragged(event) {
+    d3.select(this).attr("transform", "translate("+event.x+","+event.y+")");
+    
+
+}
+
+
+function dragended(event) {
+  const another_x= d3.select("#g1").attr("transform").match(/(?<=\()\d{1,}(?=\,)/);
+  const another_y= d3.select("#g1").attr("transform").match(/\d{1,}(?=\))/);
+  const a=another_x-event.x;
+  const b=another_y-event.y;
+  var distance=Math.sqrt( a*a + b*b );
+  console.log(another_x);
+  console.log(another_y);
+  console.log(distance);
+  if (distance<Math.abs(outRadius+outRadius1)){
+    d3.select(this).attr("transform", "translate("+d3.select("#g1").attr("transform").match(/(?<=\()\d{1,}(?=\,)/)+","+d3.select("#g1").attr("transform").match(/\d{1,}(?=\))/)+")");
   }
+  d3.select(this).classed("active", false);
+}

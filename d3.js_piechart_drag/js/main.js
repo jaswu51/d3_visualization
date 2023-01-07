38e0ae4d931dccde511c01f43a3d56a6f0d58906
jaswu51 <506346013@qq.com>
2023-01-07 21:23:09 +0100

    push

--- d3.js_piechart_drag/js/main.js
+++ d3.js_piechart_drag/js/main.js
@@ -15,77 +15,73 @@ const svg=d3.select("#piechart")
   .attr("width", width)
   .attr("height", height);
 
-const pieData = d3.pie()
+  const pieData = d3.pie()
     .value((d) => d.value)
     .sort((a, b) => a.value - b.value)(data);
 
-const arc = d3.arc().innerRadius(0).outerRadius(outRadius);
-const arc1 = d3.arc().innerRadius(0).outerRadius(outRadius1);
-const color = d3.scaleOrdinal(d3.schemeCategory10);
-const color1 = d3.scaleLinear().domain([1,3])
-.range(["green", "yellow"]);
+const arc1 = d3.arc().innerRadius(0).outerRadius(outRadius);
+const arc2 = d3.arc().innerRadius(0).outerRadius(outRadius1);
+const label=d3.arc().innerRadius(outRadius-40).outerRadius(outRadius-40);
+
+const color1 = d3.scaleOrdinal(d3.schemeCategory10);
+const color2 = d3.scaleLinear().domain([1,3]).range(["green", "yellow"]);
+
+const g1=svg.append("g").attr("id", "g1").attr("transform", "translate("+width/2+","+height/2+")").selectAll().data(pieData).enter();
+g1.append("path")
+  .attr("d",arc1)
+  .attr("fill", (d, i) => color1(i))
+;
+
+g1.append("text")
+    .attr("transform", function(d){return "translate(" + label.centroid(d) + ")"; })
+    .style("font-size","10px")
+    .attr("dy", "0.35em")
+    .attr('text-anchor', 'middle')
+    .text(function(d,i) {return data[i].name; });
 
 
 
-const arcGroup = svg.append("g")
-    .attr("id", "g1")
-    .attr("transform", "translate(300, 300)")
-    .selectAll("path")
-    .data(pieData);
-
-
-arcGroup.join("path")
-    .attr("fill", (d, i) => color(i))
-    .attr("d", arc);
-
-const arcGroup1 = svg.append("g")
-    .attr("id", "g2")
-    .attr("transform", "translate(120, 100)")
-    .call(
+    const g2=svg.append("g").attr("id", "g2").attr("transform", "translate("+width/5+","+height/5+")").call(
       d3.drag()
     .on("start", dragstarted)
     .on("drag", dragged)
     .on("end", dragended)
     )
-    .selectAll("path")
-    .data(pieData)
+    .selectAll().data(pieData).enter();
+
+    g2.append("path")
+      .attr("d",arc2)
+      .attr("fill", (d, i) => color2(i))
     ;
-arcGroup1.join("path")
-    .attr("fill", (d, i) => color1(i))
-    .attr("d", arc1)
-    ;
-
-function update() {
-  voronoi = d3.Delaunay.from(g, d => d.x, d => d.y).voronoi([0, 0, width, height]);
-  circle.attr("transform", d => `translate(${d.x},${d.y})`);
-  cell.attr("d", (d, i) => voronoi.renderCell(i));
-}
-
-
-
-
-function dragstarted(_event) {
-    d3.select(this).raise().classed("active", true);
-}
-
-function dragged(event) {
-    d3.select(this).attr("transform", "translate("+event.x+","+event.y+")");
     
-
-}
-
-
-function dragended(event) {
-  const another_x= d3.select("#g1").attr("transform").match(/(?<=\()\d{1,}(?=\,)/);
-  const another_y= d3.select("#g1").attr("transform").match(/\d{1,}(?=\))/);
-  const a=another_x-event.x;
-  const b=another_y-event.y;
-  var distance=Math.sqrt( a*a + b*b );
-  console.log(another_x);
-  console.log(another_y);
-  console.log(distance);
-  if (distance<Math.abs(outRadius+outRadius1)){
-    d3.select(this).attr("transform", "translate("+d3.select("#g1").attr("transform").match(/(?<=\()\d{1,}(?=\,)/)+","+d3.select("#g1").attr("transform").match(/\d{1,}(?=\))/)+")");
-  }
-  d3.select(this).classed("active", false);
-}
+    g2.append("text")
+        .attr("transform", function(d){return "translate(" + label.centroid(d) + ")"; })
+        .style("font-size","10px")
+        .attr("dy", "0.35em")
+        .attr('text-anchor', 'middle')
+        .text(function(d,i) {return data[i].name; });
+        function dragstarted(_event) {
+          d3.select(this).raise().classed("active", true);
+      }
+      
+      function dragged(event) {
+          d3.select(this).attr("transform", "translate("+event.x+","+event.y+")");
+          
+      
+      }
+      
+      
+      function dragended(event) {
+        const another_x= d3.select("#g1").attr("transform").match(/(?<=\()\d{1,}(?=\,)/);
+        const another_y= d3.select("#g1").attr("transform").match(/\d{1,}(?=\))/);
+        const a=another_x-event.x;
+        const b=another_y-event.y;
+        var distance=Math.sqrt( a*a + b*b );
+        console.log(another_x);
+        console.log(another_y);
+        console.log(distance);
+        if (distance<Math.abs(outRadius+outRadius1)){
+          d3.select(this).attr("transform", "translate("+d3.select("#g1").attr("transform").match(/(?<=\()\d{1,}(?=\,)/)+","+d3.select("#g1").attr("transform").match(/\d{1,}(?=\))/)+")");
+        }
+        d3.select(this).classed("active", false);
+      }
